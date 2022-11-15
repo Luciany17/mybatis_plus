988ec207dfc1f3cbd8d7bb34fbb7021c12a0ea23
Luciany <2315563495@qq.com>
2022-11-15 19:29:26 +0800

    hot第一次提交

--- src/main/java/com/example/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/com/example/mybatis_plus/MybatisPlusApplication.java
@@ -12,6 +12,7 @@ public class MybatisPlusApplication {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
+        System.out.println("hello,git4");
         SpringApplication.run(MybatisPlusApplication.class, args);
     }
 
